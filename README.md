ee30a35ea9bea43fbba0606b686d0bcb14028002
Johny Marques <johny.ribeiro@ufu.br>
2022-08-26 15:55:52 -0300

    following testing section.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Following a blog about an OS in Rust
 I'm following the tutorial bellow, to implement a simple operational system using Rust Lang.
 
 ## https://os.phil-opp.com/
+
+
+Command to run OS:
+cargo run -Z build-std-features=compiler-builtins-mem
+
+Command to run tests:
+cargo test -Z build-std-features=compiler-builtins-mem  
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -18,4 +23,37 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
