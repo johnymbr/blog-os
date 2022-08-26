904c8f3de114edcfd3276ecf9d4649267d9eecfe
Johny Marques <johny.ribeiro@ufu.br>
2022-08-26 14:27:12 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    panic!("Some panic message");
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
