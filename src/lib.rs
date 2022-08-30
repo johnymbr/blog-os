14e0de3c98969fe407bff3d98a2a89b84f093a1d
Johny Marques <johny.ribeiro@ufu.br>
2022-08-30 14:56:42 -0300

    adding a folder for our integration tests and adding a lib.rs to use some test functions in our integration tests.

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T 
+where T: Fn() {
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
